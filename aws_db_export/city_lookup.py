006759f7ba92415a2ec55592024f2bdb492dee17
keabraekman <kea.braekman@mail.com>
2021-11-17 15:26:11 -0800

    Exported a third time with aws cli, easier with json format

--- aws_db_export/city_lookup.py
+++ aws_db_export/city_lookup.py
@@ -1,3 +1,5 @@
+import json
+
 f = open('../1000_largest_cities.txt', 'r')
 content = f.read()
 # print(content)
@@ -5,4 +7,14 @@ content = f.read()
 cities = content.split(',')[1::4]
 states = content.split(',')[2::4]
 
-print(cities)
+# print(cities)
+
+
+g = open('export2/data.json', 'r')
+export = json.load(g)
+
+
+print(type(export))
+
+
+# cities_exported = export.split
